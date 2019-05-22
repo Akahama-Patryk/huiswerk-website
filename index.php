334eab2eb1b3deef833f7ac3e72a0a971d8f52ec
Akahama <orel971111@gmail.com>
2019-05-22 14:15:41 +0200

    Fuck you Milan

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 	include_once('classes/Autoloader.php');
 	Session::start();
 ?>
-
+<html lang="nl">
+<head>
+	<title>Project Huiswerk</title>
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+</head>
+<body>
+<?=Homework::displayHomework(Homework::fetchHomework())?>
+</body>
+</html>
