2bd1648e454086dc70f7b3b0cec68627fe230162
Akahama-Patryk <orel971111@gmail.com>
2019-10-03 13:54:22 +0200

    Revert

--- classes/User.php
+++ classes/User.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 	/**
 	 * Class User
 	 */
@@ -10,13 +10,13 @@
 		 * string Username.
 		 */
 		protected $username;
-		
+
 		/**
 		 * @var
 		 *  string Password.
 		 */
 		protected $pass;
-		
+
 		/**
 		 * User constructor.
 		 * @param $username
@@ -27,24 +27,24 @@
 			$this->username = $username;
 			$this->pass = $pass;
 		}
-		
+
 		/**
 		 * Checks input data at login screen against user table, sets session username variable and/or admin status variable.
 		 */
 		public function login()
 		{
-			
+
 			if (!empty($this->username) && !empty($this->pass)) {
 				$conn = Utility::pdoConnect();
-				
+
 				$login = $conn->prepare("select * from user where username=:username");
 				$login->bindParam(':username', $this->username);
 				$login->execute();
-				
+
 				if ($login->rowCount() == 1) {
 					$user_data = $login->fetch(PDO::FETCH_ASSOC);
 					$hashed_pass = $user_data['password'];
-					
+
 					if (User::verifyEncryptedPassword($this->pass, $hashed_pass)) {
 						// Password verified, user has been logged in.
 						$_SESSION['login_status'] = true;
@@ -59,16 +59,16 @@
 			}
 			return false;
 		}
-		
+
 		/**
 		 * @return bool
-		 *  Adds user to db.
+		 *  Registers users and encrypts password using hash
 		 */
 		public function register()
 		{
 			if (!empty($this->username) && !empty($this->pass)) {
 				$conn = Utility::pdoConnect();
-				$this->pass = User::encryptPassword($this->pass, "PASSWORD_DEFAULT");
+				$this->pass = User::encryptPassword($this->pass);
 				$register = $conn->prepare("INSERT INTO user (id,username,password) VALUES  ((SELECT UUID()), ?, ?)");
 				$register->bindParam(1, $this->username);
 				$register->bindParam(2, $this->pass);
@@ -78,27 +78,33 @@
 				return false;
 			}
 		}
-		
+
+		/**
+		 * @param $user_id
+		 *  int User id.
+		 *
+		 * Removes user from user table in database.
+		 */
+		public function remove($user_id)
+		{
+
+		}
+
 		/**
 		 * @param $pass
-		 *  string Password to encrypt.
-		 * @param $algorithm
-		 *  string Encryption algorithm to use.
 		 * @return mixed
 		 *
 		 * Encrypts passwords.
 		 */
-		public static function EncryptPassword($pass, $algorithm)
+		public static function EncryptPassword($pass)
 		{
-			$hashed_pass = password_hash($pass, $algorithm);
+			$hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
 			return $hashed_pass;
 		}
-		
+
 		/**
 		 * @param $pass
-         *  string Password to check.
 		 * @param $hashed_pass
-         *  string Hashed value to check against.
 		 * @return bool
 		 *
 		 * Verifies encrypted passwords.
@@ -111,11 +117,11 @@
 				return false;
 			}
 		}
-		
+
 		/**
 		 * @return false|PDOStatement
 		 *
-		 * Fetches table user from db.
+		 * Fetches table user from database.
 		 */
 		public static function fetchUsers()
 		{
@@ -123,7 +129,7 @@
 			$data = $conn->query("select * from user");
 			return $data;
 		}
-		
+
 		/**\
 		 * @param $data
 		 *
@@ -136,116 +142,25 @@
                 <table class='table table-sm'>
                     <thead class='thead-light'>
                     <tr>
-                        <td><?= $row['username'] ?></td>
-                        <td><a href="gebruiker/wijzigen.php?user_id=<?= $row['id'] ?>">
-                                <i class="far fa-edit"></i>
-                            </a></td>
-                        <td><a href="dashboardhandler.php?user_del_id=<?= $row['id'] ?>">
-                                <i class="fas fa-times"></i>
-                            </a></td>
+                        <th scope='col'>Naam</th>
+                        <th scope='col'>Wijzig</th>
+                        <th scope='col'>Verwijder</th>
                     </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-        <?php
-    }
-
-    /**
-     * @param $userID
-     * @return mixed $user_data
-     * Fetch an user information based on userID from dashboard_admin.
-     */
-    public static function fetchUser($userID)
-    {
-        if (!empty($userID)) {
-            $conn = Utility::pdoConnect();
-
-            $fetch = $conn->prepare("select username from user where id = :userID");
-            $fetch->bindParam(':userID', $userID);
-            $fetch->execute();
-            if ($fetch->rowCount() == 1) {
-                $user_data = $fetch;
-                return $user_data;
-            }
-        }
-    }
-
-    /**
-     * Checks input data at login screen against user table, sets session username variable and/or admin status variable.
-     */
-    public function login()
-    {
-
-        if (!empty($this->username) && !empty($this->pass)) {
-            $conn = Utility::pdoConnect();
-
-            $login = $conn->prepare("select * from user where username=:username");
-            $login->bindParam(':username', $this->username);
-            $login->execute();
-
-            if ($login->rowCount() == 1) {
-                $user_data = $login->fetch(PDO::FETCH_ASSOC);
-                $hashed_pass = $user_data['password'];
-
-                if (User::verifyEncryptedPassword($this->pass, $hashed_pass)) {
-                    // Password verified, user has been logged in.
-                    $_SESSION['login_status'] = true;
-                    $_SESSION['login_user'] = $user_data['username'];
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * @param $pass
-     * @param $hashed_pass
-     * @return bool
-     *
-     * Verifies encrypted passwords.
-     */
-    public static function VerifyEncryptedPassword($pass, $hashed_pass)
-    {
-        if (password_verify($pass, $hashed_pass)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @return bool
-     *  Registers users and encrypts password using hash
-     */
-    public function register()
-    {
-        if (!empty($this->username) && !empty($this->pass)) {
-            $conn = Utility::pdoConnect();
-            $this->pass = User::encryptPassword($this->pass);
-            $register = $conn->prepare("INSERT INTO user (id,username,password) VALUES  ((SELECT UUID()), ?, ?)");
-            $register->bindParam(1, $this->username);
-            $register->bindParam(2, $this->pass);
-            $register->execute();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @param $user_id
-     *  int User id.
-     *
-     * Removes user from user table in database.
-     */
-    public function remove($user_id)
-    {
-
-    }
-}
+                    <tbody>
+					<?php foreach ($data as $row) : ?>
+                        <tr>
+                            <td><?= $row['username'] ?></td>
+                            <td><a href="gebruiker/wijzigen.php<?= $row['id'] ?>">
+                                    <i class="far fa-edit"></i>
+                                </a></td>
+                            <td><a href="dashboardhandler.php?user_del_id=<?= $row['id'] ?>">
+                                    <i class="fas fa-times"></i>
+                                </a></td>
+                        </tr>
+					<?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+			<?php
+		}
+	}
